665f16ade1092a6e93c24ba544ffe4bf965a18d5
Ninad Kheratkar <ninadkheratkar51@gmail.com>
2023-06-07 12:09:22 -0500

    First commit

---
+++ src/components/task-manager/Editable.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let value;
+	let isEditing = false;
+</script>
+
+{#if isEditing}
+	<div class="flex-it">
+		<textarea bind:value />
+		<button
+			on:click={() => {
+				isEditing = false;
+				dispatch('editCancel', {
+					taskText: value
+				});
+			}}
+			class="flex underline"
+			>Close
+		</button>
+	</div>
+{:else}
+	<button on:click={() => (isEditing = true)}>
+		<slot />
+	</button>
+{/if}
---
+++ src/stores/tasks.js
@@ -0,0 +1,64 @@
+import { writable, get } from 'svelte/store';
+
+const DEFAUL_DATA = [
+	{
+		id: 'l-1',
+		text: 'List 1',
+		items: [
+			{ id: 't-1', text: 'Task 1' },
+			{ id: 't-2', text: 'Task 2' },
+			{ id: 't-3', text: 'Task 3' }
+		]
+	},
+	{
+		id: 'l-2',
+		text: 'List 2',
+		items: [
+			{ id: 't-4', text: 'Task 4' },
+			{ id: 't-5', text: 'Task 5' },
+			{ id: 't-6', text: 'Task 6' }
+		]
+	},
+	{
+		id: 'l-3',
+		text: 'List 3',
+		items: [
+			{ id: 't-7', text: 'Task 7' },
+			{ id: 't-8', text: 'Task 8' },
+			{ id: 't-9', text: 'Task 9' }
+		]
+	}
+];
+
+function createStore() {
+	const taskList = writable(DEFAUL_DATA);
+	const { subscribe } = taskList;
+
+	return {
+		subscribe,
+		updateTask: (task, listIdx) => {
+			// const taskIdx = get(taskList)[listIdx].items.findIndex(item => item.id === task.id);
+
+			// if (taskIdx > -1) {
+			//   taskList.update(list => {
+			//     list[listIdx].items[taskIdx] = {...task};
+			//     return list;
+			//   })
+			// }
+
+			// -------
+
+			taskList.update((list) => {
+				const taskIdx = list[listIdx].items.findIndex((item) => item.id === task.id);
+
+				if (taskIdx > -1) {
+					list[listIdx].items[taskIdx] = { ...task };
+				}
+
+				return list;
+			});
+		}
+	};
+}
+
+export const taskListStore = createStore();
